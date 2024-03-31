d1b070cef684281520b7bbafcb0b20db1354b6a2
matheusoliveirajc <eumatheusdeoliveirajc@gmail.com>
2024-03-30 21:19:23 -0300

    rt

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
 const Link_button = document.querySelectorAll('.link_button');
+
 function RemoveClass(){
     for(let i = 0; i < Link_button.length; i++){
         Link_button[i].classList.remove('link-active');
     }
 }
-Link_button[0].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
-});
-Link_button[1].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
-});
-Link_button[2].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
-});
-Link_button[3].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
-});
-Link_button[4].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
-});
-Link_button[5].addEventListener('click', function(){
-    RemoveClass();
-    if(this.classList != 'link-active'){
-        this.classList.add('link-active');
-    }
+
+Link_button.forEach(button => {
+    button.addEventListener('click', function(){
+        RemoveClass();
+        if(!this.classList.contains('link-active')){
+            this.classList.add('link-active');
+        }
+    });
 });
